ae4e1b0bf450ba62075c4cabcb093fa37b80a82b
turkmenoglubugra <bugrakaanturkmenoglu@gmail.com>
2020-06-07 00:31:06 +0300

    -Performans iyileştirmeleri

--- app/src/main/java/com/tr/sifreUretici/MainActivity.java
+++ app/src/main/java/com/tr/sifreUretici/MainActivity.java
@@ -3,12 +3,71 @@ package com.tr.sifreUretici;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class MainActivity extends AppCompatActivity {
+    Button generate;
+    TextView password, length;
+    SeekBar seekBar;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        length = findViewById(R.id.editText);
+        password = findViewById(R.id.editText2);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                length.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        generate = findViewById(R.id.button);
+        generate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String pw = passwordGenerator.process(Integer.valueOf(length.getText().toString().trim()));
+                password.setText(pw.trim());
+            }
+        });
+
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
---
+++ app/src/main/java/com/tr/sifreUretici/passwordGenerator.java
@@ -0,0 +1,16 @@
+package com.tr.sifreUretici;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class passwordGenerator {
+
+    private static final int MIN_CODE = 33, MAX_CODE = 126;
+
+    public static String process(int length) {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < length; i++){
+            builder.append((char) ThreadLocalRandom.current().nextInt(MIN_CODE, MAX_CODE +1 ));
+        }
+        return builder.toString();
+    }
+}
