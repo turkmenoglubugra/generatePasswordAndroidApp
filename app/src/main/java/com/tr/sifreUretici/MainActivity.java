0d3e3d0a8cbb1d347fea72d51d6df9825b559cc9
turkmenoglubugra <bugrakaanturkmenoglu@gmail.com>
2020-06-07 17:36:40 +0300

    -Performans iyileştirmeleri

--- app/src/main/java/com/tr/sifreUretici/MainActivity.java
+++ app/src/main/java/com/tr/sifreUretici/MainActivity.java
@@ -2,9 +2,13 @@ package com.tr.sifreUretici;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -12,22 +16,46 @@ import android.widget.TextView;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class MainActivity extends AppCompatActivity {
-    Button generate;
+    Button generate, copy;
     TextView password, length;
     SeekBar seekBar;
     private AdView mAdView;
+    private CheckBox checkBox, checkBox2, checkBox3, checkBox4;
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-2814589180123732/3699821371");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
 
+        });
+        checkBox = findViewById(R.id.checkBox);
+        checkBox2 = findViewById(R.id.checkBox2);
+        checkBox3 = findViewById(R.id.checkBox3);
+        checkBox4 = findViewById(R.id.checkBox4);
+
+        checkBox.setChecked(true);
+        checkBox2.setChecked(true);
+        checkBox3.setChecked(true);
+        checkBox4.setChecked(true);
 
         length = findViewById(R.id.editText);
         password = findViewById(R.id.editText2);
@@ -55,11 +83,36 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                String pw = passwordGenerator.process(Integer.valueOf(length.getText().toString().trim()));
-                password.setText(pw.trim());
+                if(checkBox.isChecked() || checkBox2.isChecked() || checkBox3.isChecked() || checkBox4.isChecked()){
+                    String pw = passwordGenerator.process(Integer.valueOf(length.getText().toString().trim()), checkBox2.isChecked(), checkBox.isChecked(), checkBox3.isChecked(), checkBox4.isChecked());
+                    password.setText(pw.trim());
+                } else {
+                    new SweetAlertDialog(MainActivity.this, SweetAlertDialog.ERROR_TYPE)
+                            .setTitleText("Oops...")
+                            .setContentText("Please, Select an option at least!")
+                            .show();
+                    return;
+                }
             }
         });
-
+        copy = findViewById(R.id.button21);
+        copy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("label", password.getText().toString().trim());
+                clipboard.setPrimaryClip(clip);
+                new SweetAlertDialog(MainActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                        .setTitleText("Succes...")
+                        .setContentText("Copied to clipboard")
+                        .show();
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.");
+                }
+            }
+        });
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
--- app/src/main/java/com/tr/sifreUretici/passwordGenerator.java
+++ app/src/main/java/com/tr/sifreUretici/passwordGenerator.java
@@ -1,16 +1,30 @@
 package com.tr.sifreUretici;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class passwordGenerator {
 
     private static final int MIN_CODE = 33, MAX_CODE = 126;
+    private static String allowedCharacters;
 
-    public static String process(int length) {
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < length; i++){
-            builder.append((char) ThreadLocalRandom.current().nextInt(MIN_CODE, MAX_CODE +1 ));
+
+
+
+    public static String process(int length, Boolean capitalLetter, Boolean lowercaseLetter, Boolean numbers, Boolean symbols) {
+        allowedCharacters = "";
+        if (capitalLetter) {allowedCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";}
+        if (lowercaseLetter) {allowedCharacters += "abcdefghijklmnopqrstuvwxyz";}
+        if (numbers) {allowedCharacters += "0123456789";}
+        if (symbols) {allowedCharacters += "!@#$%^&*()_-+=<>?/{}~|";}
+
+        final Random random=new Random();
+        final StringBuilder sb=new StringBuilder(length);
+
+        for(int i=0;i<length;++i){
+            sb.append(allowedCharacters.charAt(random.nextInt(allowedCharacters.length())));
+
         }
-        return builder.toString();
+        return sb.toString();
     }
 }
